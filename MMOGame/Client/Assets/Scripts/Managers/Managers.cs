7e68c9eeb43406f845926b39ad646dc170943170
thebiggestmonstro <monstroowner@gmail.com>
2025-06-19 01:41:59 +0900

    Small Fix

--- MMOGame/Client/Assets/Scripts/Managers/Managers.cs
+++ MMOGame/Client/Assets/Scripts/Managers/Managers.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    static Managers s_instance; 
-    static Managers Instance { get { Init(); return s_instance; } }
+    static Managers s_instance; // 유일성이 보장된다
+    static Managers Instance { get { Init(); return s_instance; } } // 유일한 매니저를 갖고온다
 
     #region Contents
     MapManager _map = new MapManager();
-    ObjectManager _obj = new ObjectManager(); 
+    ObjectManager _obj = new ObjectManager();
+    NetworkManager _network = new NetworkManager();
 
     public static MapManager Map { get { return Instance._map; } }
     public static ObjectManager Object { get { return Instance._obj; } }
+    public static NetworkManager Network { get { return Instance._network; } }
     #endregion
 
     #region Core
@@ -29,23 +31,23 @@ public class Managers : MonoBehaviour
     public static SceneManagerEx Scene { get { return Instance._scene; } }
     public static SoundManager Sound { get { return Instance._sound; } }
     public static UIManager UI { get { return Instance._ui; } }
-	#endregion
+    #endregion
 
-	void Start()
+    void Start()
     {
         Init();
-	}
+    }
 
     void Update()
     {
-
+        _network.Update();
     }
 
     static void Init()
     {
         if (s_instance == null)
         {
-			GameObject go = GameObject.Find("@Managers");
+            GameObject go = GameObject.Find("@Managers");
             if (go == null)
             {
                 go = new GameObject { name = "@Managers" };
@@ -55,11 +57,12 @@ public class Managers : MonoBehaviour
             DontDestroyOnLoad(go);
             s_instance = go.GetComponent<Managers>();
 
+            s_instance._network.Init();
             s_instance._data.Init();
             s_instance._pool.Init();
             s_instance._sound.Init();
-        }		
-	}
+        }
+    }
 
     public static void Clear()
     {
