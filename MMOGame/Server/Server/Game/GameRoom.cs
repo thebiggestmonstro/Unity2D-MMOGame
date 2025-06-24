1dda6f9693930edb374a448f371316d31f9af5e9
thebiggestmonstro <monstroowner@gmail.com>
2025-06-24 21:07:18 +0900

    Enter Game

---
+++ MMOGame/Server/Server/Game/GameRoom.cs
@@ -0,0 +1,84 @@
+﻿using Google.Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Game
+{
+    public class GameRoom
+    {
+        object _lock = new object();
+        public int RoomId { get; set; }
+
+        List<Player> _players = new List<Player>();
+
+        public void EnterGame(Player newPlayer)
+        {
+            if (newPlayer == null)
+                return;
+
+            lock (_lock) 
+            {
+                _players.Add(newPlayer);
+                newPlayer.Room = this;
+
+                // 입장한 본인에게 정보 전송
+                {
+                    S_EnterGame enterPacket = new S_EnterGame();
+                    enterPacket.Player = newPlayer.Info;
+                    newPlayer.Session.Send(enterPacket);
+
+                    // 입장한 나의 Spawn 여부를 다른 플레이어들에게 전송
+                    S_Spawn spawnPacket = new S_Spawn();
+                    foreach (Player p in _players)
+                    {
+                        if (newPlayer != p)
+                            spawnPacket.Players.Add(p.Info);
+                    }
+                    newPlayer.Session.Send(spawnPacket);
+                }
+
+                // 인접한 타인에게 정보 전송
+                {
+                    S_Spawn spawnPacket = new S_Spawn();
+                    spawnPacket.Players.Add(newPlayer.Info);
+                    foreach (Player p in _players)
+                    {
+                        if (newPlayer != p)
+                            p.Session.Send(spawnPacket);
+                    }
+                }                
+            }
+        }
+
+        public void LeaveGame(int playerId)
+        {
+            lock (_lock)
+            {
+                Player player = _players.Find(p => p.Info.PlayerId == playerId);
+                if (player == null)
+                    return;
+
+                _players.Remove(player);
+                player.Room = null;
+
+                // 퇴장한 본인에게 정보 전송
+                {
+                    S_LeaveGame leavePacket = new S_LeaveGame();
+                    player.Session.Send(leavePacket);
+                }
+
+                // 퇴장한 본인의 정보를 타인에게 전송
+                {
+                    S_Despawn despawnPacket = new S_Despawn();
+                    despawnPacket.PlayerIds.Add(player.Info.PlayerId);
+                    foreach (Player p in _players)
+                    { 
+                        if(player != p)
+                            p.Session.Send(despawnPacket);
+                    }
+                }
+            }
+        }
+    }
+}
--- MMOGame/Server/Server/Session/ClientSession.cs
+++ MMOGame/Server/Server/Session/ClientSession.cs
@@ -8,12 +8,14 @@ using ServerCore;
 using System.Net;
 using Google.Protobuf.Protocol;
 using Google.Protobuf;
+using Server.Game;
 
 namespace Server
 {
-	class ClientSession : PacketSession
+	public class ClientSession : PacketSession
 	{
 		public int SessionId { get; set; }
+		public Player MyPlayer { get; set; }
 
 		public void Send(IMessage packet)
 		{
@@ -22,8 +24,7 @@ namespace Server
 
             ushort size = (ushort)packet.CalculateSize();
             byte[] sendBuffer = new byte[size + 4];
-            Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
-            ushort protocolId = (ushort)MsgId.SChat;
+            Array.Copy(BitConverter.GetBytes((ushort)(size + 4)), 0, sendBuffer, 0, sizeof(ushort));
             Array.Copy(BitConverter.GetBytes((ushort)msgId), 0, sendBuffer, 2, sizeof(ushort));
             Array.Copy(packet.ToByteArray(), 0, sendBuffer, 4, size);
 
@@ -35,18 +36,15 @@ namespace Server
 			Console.WriteLine($"OnConnected : {endPoint}");
 
 			// PROTO Test
-			S_Chat chat = new S_Chat()
+			MyPlayer = PlayerManager.Instance.Add();
 			{
-				Context = "안녕하세요"
-			};
+				MyPlayer.Info.Name = $"Player_{MyPlayer.Info.PlayerId}";
+				MyPlayer.Info.PosX = 0;
+				MyPlayer.Info.PosY = 0;
+				MyPlayer.Session = this;
+			}
 
-			Send(chat);
-
-			//S_Chat chat2 = new S_Chat();
-			//chat2.MergeFrom(sendBuffer, 4, sendBuffer.Length - 4);
-			//////////////////////////
-			//////////////////////////
-			//Program.Room.Push(() => Program.Room.Enter(this));
+			RoomManager.Instance.Find(1).EnterGame(MyPlayer);
 		}
 
 		public override void OnRecvPacket(ArraySegment<byte> buffer)
@@ -56,7 +54,9 @@ namespace Server
 
 		public override void OnDisconnected(EndPoint endPoint)
 		{
-			SessionManager.Instance.Remove(this);
+            RoomManager.Instance.Find(1).LeaveGame(MyPlayer.Info.PlayerId);
+
+            SessionManager.Instance.Remove(this);
 
 			Console.WriteLine($"OnDisconnected : {endPoint}");
 		}
