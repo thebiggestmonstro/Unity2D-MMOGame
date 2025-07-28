1ba277deb750b915cbdab64b0a5689a8c8ca20b3
thebiggestmonstro <monstroowner@gmail.com>
2025-07-28 13:17:36 +0900

    Command Pattern

---
+++ MMOGame/Server/Server/Game/Job/Job.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Game.Job
+{
+    public interface IJob
+    {
+        void Execute();
+    }
+
+    // 인자가 없는 패킷을 저장하는 Job
+    public class Job : IJob
+    {
+        Action _action;
+
+        // 생성자에서 패킷을 받아 저장
+        public Job(Action action)
+        {
+            _action = action;
+        }
+
+        // 인자가 없는 패킷 처리 요청
+        public void Execute() 
+        { 
+            _action.Invoke();
+        }
+    }
+
+    // 인자를 1개 사용하는 패킷을 저장하는 Job
+    public class Job<T1> : IJob
+    {
+        Action<T1> _action;
+        T1 _t1;
+
+        // 생성자에서 패킷을 받아 저장
+        public Job(Action<T1> action, T1 t1)
+        {
+            _action = action;
+            _t1 = t1;
+        }
+
+        // 인자가 1개 있는 패킷 처리 요청
+        public void Execute()
+        {
+            _action.Invoke(_t1);
+        }
+    }
+
+    // 인자를 2개 사용하는 패킷을 저장하는 Job
+    public class Job<T1, T2> : IJob
+    {
+        Action<T1, T2> _action;
+        T1 _t1;
+        T2 _t2;
+
+        // 생성자에서 패킷을 받아 저장
+        public Job(Action<T1, T2> action, T1 t1, T2 t2)
+        {
+            _action = action;
+            _t1 = t1;
+            _t2 = t2;
+        }
+
+        // 인자가 2개 있는 패킷 처리 요청
+        public void Execute()
+        {
+            _action.Invoke(_t1, _t2);
+        }
+    }
+
+    // 인자를 3개 사용하는 패킷을 저장하는 Job
+    public class Job<T1, T2, T3> : IJob
+    {
+        Action<T1, T2, T3> _action;
+        T1 _t1;
+        T2 _t2;
+        T3 _t3;
+
+        // 생성자에서 패킷을 받아 저장
+        public Job(Action<T1, T2, T3> action, T1 t1, T2 t2, T3 t3)
+        {
+            _action = action;
+            _t1 = t1;
+            _t2 = t2;
+            _t3 = t3;
+        }
+
+        // 인자가 3개 있는 패킷 처리 요청
+        public void Execute()
+        {
+            _action.Invoke(_t1, _t2, _t3);
+        }
+    }
+}
---
+++ MMOGame/Server/Server/Game/Job/JobSerializer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Game.Job
+{
+    public class JobSerializer
+    {
+        Queue<IJob> _jobQueue = new Queue<IJob>();
+        object _lock = new object();
+        bool _flush = false;
+
+        // 외부에서 호출하여 패킷을 저장하는 함수
+        public void Push(Action action) { Push(new Job(action)); }
+        public void Push<T1>(Action<T1> action, T1 t1) { Push(new Job<T1>(action, t1)); }
+        public void Push<T1, T2>(Action<T1, T2> action, T1 t1, T2 t2) { Push(new Job<T1, T2>(action, t1, t2)); }
+        public void Push<T1, T2, T3>(Action<T1, T2, T3> action, T1 t1, T2 t2, T3 t3) { Push(new Job<T1, T2, T3>(action, t1, t2, t3)); }
+
+        // 패킷 저장
+        public void Push(IJob job)
+        {
+            bool flush = false;
+
+            lock (_lock)
+            {
+                _jobQueue.Enqueue(job);
+                // 만약 들어온 패킷이 큐의 Head에 저장되었다면 바로 처리
+                if (_flush == false)
+                    flush = _flush = true;
+            }
+
+            if (flush)
+                Flush();
+        }
+
+        // 패킷 제거
+        IJob Pop()
+        {
+            lock (_lock)
+            {
+                if (_jobQueue.Count == 0)
+                {
+                    _flush = false;
+                    return null;
+                }
+                return _jobQueue.Dequeue();
+            }
+        }
+
+        // 저장된 패킷 처리
+        void Flush()
+        {
+            while (true)
+            {
+                IJob job = Pop();
+                if (job == null)
+                    return;
+
+                job.Execute();
+            }
+        }
+    }
+}
