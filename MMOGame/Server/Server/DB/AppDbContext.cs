488367a9fe9a88c344afbab4abefcf4ce5cb2463
thebiggestmonstro <monstroowner@gmail.com>
2025-09-01 10:54:07 +0900

    First Step

---
+++ MMOGame/Server/Server/DB/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<AccountDb> Accounts { get; set; }
+        public DbSet<PlayerDb> Players { get; set; }
+
+        string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=GameDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        static readonly ILoggerFactory _logger = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .UseSqlServer(ConfigManager.Config == null ? _connectionString : ConfigManager.Config.connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AccountDb>()
+                .HasIndex(a => a.AccountName)
+                .IsUnique();
+
+            modelBuilder.Entity<PlayerDb>()
+                .HasIndex(p => p.PlayerName)
+                .IsUnique();
+        }
+    }
+}
