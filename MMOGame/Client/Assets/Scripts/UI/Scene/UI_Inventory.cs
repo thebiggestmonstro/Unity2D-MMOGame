f0ed5b150ed33d9615600b707d967fec6b52b54b
thebiggestmonstro <monstroowner@gmail.com>
2025-09-16 11:02:06 +0900

    Inventory #01

---
+++ MMOGame/Client/Assets/Scripts/UI/Scene/UI_Inventory.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Inventory : UI_Base
+{
+    public List<UI_Inventory_Item> Items { get; } = new List<UI_Inventory_Item>();
+    public override void Init()
+    {
+        Items.Clear();
+
+        GameObject grid = transform.Find("ItemGrid").gameObject;
+        foreach (Transform child in grid.transform)
+            Destroy(child.gameObject);
+
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject go = Managers.Resource.Instantiate("UI/Scene/UI_Inventory_Item", grid.transform);
+            UI_Inventory_Item item = go.GetOrAddComponent<UI_Inventory_Item>();
+            Items.Add(item);
+        }
+    }
+
+    public void RefreshUI()
+    { 
+        
+    }
+}
